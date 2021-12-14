bc48736a16db869a21bd13e345041cd80403f808
Saemkenya <187comps@gmail.com>
2021-12-14 16:54:31 +0300

    Added Dockerfile

---
+++ src/pages/SignUp/FormSuccessJs.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { FormContentRight, FormSuccessH1, FormImg2 } from "./Form.elements";
+import Confirmed from "../../images/Confirmed";
+
+const FormSuccess = () => {
+  return (
+    <FormContentRight>
+      <FormSuccessH1>Request received by Kanmaa Adventures!</FormSuccessH1>
+      <FormImg2>
+        <Confirmed height={500} width={400} />
+      </FormImg2>
+    </FormContentRight>
+  );
+};
+
+export default FormSuccess;
